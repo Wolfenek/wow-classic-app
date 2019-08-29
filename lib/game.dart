8d1e8b44efdc0a077f59d5e360c57ad2687441e1
Wolfenek <wolfenek@gmail.com>
2019-08-29 17:24:16 +0200

    building layout on 2nd view (work in progress)

--- lib/game.dart
+++ lib/game.dart
@@ -3,6 +3,49 @@ import 'package:flutter/material.dart';
 class Game extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('2nd route');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      backgroundColor: Colors.indigo[900],
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Get ready to rumbww22le',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.grey[100],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    height: 270.0,
+//                    color: Colors.grey[300],
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    ),
+                    child: Text(
+                      'Questions will com he dssdfsdfsdfdsfssdasdasddfsdfsdfdsre',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
